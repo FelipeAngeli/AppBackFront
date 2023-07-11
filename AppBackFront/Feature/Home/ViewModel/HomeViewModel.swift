1c7973f47659be9a4f80a6e34476c67b90f0a8c8
felipeangeli <felipe.angeli@gmail.com>
2023-07-10 21:57:23 -0300

    alamofire image

--- AppBackFront/Feature/Home/ViewModel/HomeViewModel.swift
+++ AppBackFront/Feature/Home/ViewModel/HomeViewModel.swift
@@ -72,5 +72,21 @@ class HomeViewModel{
     public  var sizeForItemAt: CGSize {
         return CGSize(width: 100, height: 34)
     }
+    
+    
+    //MARK: - NftTableViewCell
+
+    public var  numberOfRowsInSection: Int {
+        return nftData?.nftList?.count ?? 0    //MARK: - FIlterCollectionView
+
+    }
+    
+    public  func loadCurrentNft(indexPath: IndexPath) -> Nft {
+        return nftData?.nftList?[indexPath.row] ?? Nft()
+    }
+    
+    public var heightForRowAt: CGFloat {
+        return 360
+    }
 
 }
