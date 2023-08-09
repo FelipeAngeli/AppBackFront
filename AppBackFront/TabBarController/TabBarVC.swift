b9154f21dd1b6096b96bbda3ea8d13fdb3672232
felipeangeli <felipe.angeli@gmail.com>
2023-08-09 19:51:44 -0300

    agora foi

--- AppBackFront/TabBarController/TabBarVC.swift
+++ AppBackFront/TabBarController/TabBarVC.swift
@@ -11,20 +11,29 @@ class TabBarVC: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTabBar()
-        
+         setupTabBar()
     }
     
     private func setupTabBar() {
         let home = UINavigationController(rootViewController: HomeVC())
-        setViewControllers([home], animated: false)
+        let wallet = UINavigationController(rootViewController: WalletVC())
+//        let profile = UINavigationController(rootViewController: ProfileVC())
+        
+        setViewControllers([home, wallet], animated: false)
         tabBar.isTranslucent = false
         tabBar.tintColor = UIColor(red: 130/255, green: 26/255, blue: 201/255, alpha: 1.0)
-        tabBar.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
         
-        guard let items = tabBar.items else {return}
+        let tabBarAppearance: UITabBarAppearance = UITabBarAppearance()
+        tabBarAppearance.configureWithDefaultBackground()
+        tabBarAppearance.backgroundColor = UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+        tabBar.standardAppearance = tabBarAppearance
+        tabBar.scrollEdgeAppearance = tabBarAppearance
+        
+        guard let items = tabBar.items else { return }
         
         items[0].image = UIImage(systemName: "trash")
+        items[1].image = UIImage(systemName: "wallet.pass")
+//        items[2].image = UIImage(systemName: "rectangle.portrait.and.arrow.right.fill")
     }
 
 }
---
+++ AppBackFront/Utils/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,116 @@
+//
+//  UIColor+Extensions.swift
+//  AppBackFront
+//
+//  Created by Felipe Angeli on 07/08/23.
+//
+
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static var tabBarTintColor: UIColor {
+        return UIColor(red: 130/255, green: 26/255, blue: 201/255, alpha: 1.0)
+    }
+    
+    static var tabBarAppearanceColor: UIColor {
+        return UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+    }
+    
+    static var backgroundColorViewCells: UIColor {
+        return UIColor(red: 26/255, green: 26/255, blue: 1/255, alpha: 1.0)
+    }
+    
+    static var backgroundColorViewHomeTableViewCell: UIColor {
+        return UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1)
+    }
+    
+    static var textBlueLabel: UIColor {
+        return  UIColor(red: 69/255, green: 191/255, blue: 229/255, alpha: 1)
+    }
+    
+    static var collectionIsSelectedFalse: UIColor {
+        return  UIColor(red: 52/255, green: 52/255, blue: 52/255, alpha: 1)
+    }
+    
+    static var collectionIsSelectedTrue: UIColor {
+        return .purple
+    }
+    static var quotationEthBackground: UIColor {
+        return  UIColor(red: 58/255, green: 52/255, blue: 57/255, alpha: 1)
+    }
+    
+    static var quotationEthBorderColor: UIColor {
+        return UIColor(red: 131/255, green: 127/255, blue: 131/255, alpha: 1)
+    }
+    
+    static var quotationEthTextColor: UIColor {
+        return UIColor(red: 255/255, green: 152/255, blue: 255/255, alpha: 1)
+    }
+   
+    static var valueDollarEntryColor: UIColor {
+        return UIColor(red: 255/255, green: 152/255, blue: 255/255, alpha: 1)
+    }
+    
+    static var hourAndDateEntryColor: UIColor {
+        return UIColor(red: 255/255, green: 152/255, blue: 255/255, alpha: 1)
+    }
+    
+    static var priceEthEntryColor: UIColor {
+        return UIColor(red: 183/255, green: 0/255, blue: 170/255, alpha: 1)
+    }
+    
+    static var valueDollarExitColor: UIColor {
+        return UIColor(red: 122/255, green: 235/255, blue: 255/255, alpha: 1)
+    }
+    
+    static var hourAndDateExitColor: UIColor {
+        return UIColor(red: 122/255, green: 235/255, blue: 255/255, alpha: 1)
+    }
+    
+    static var priceEthExitColor: UIColor {
+        return UIColor(red: 69/255, green: 191/255, blue: 229/255, alpha: 1)
+    }
+    
+    static var exitAppBorderColorButton: UIColor {
+        return UIColor(red: 207/255, green: 0/255, blue: 192/255, alpha: 1)
+    }
+    
+    static var borderColorImageUserExitApp: UIColor {
+        return UIColor(red: 255/255, green: 205/255, blue: 252/255, alpha: 1)
+    }
+    
+    static var userTextColorExitApp: UIColor {
+        return UIColor(red: 255/255, green: 152/255, blue: 255/255, alpha: 1)
+    }
+    
+    static var backgroundColorNftDetailButton: UIColor {
+        return  UIColor(red: 187/255, green: 187/255, blue: 187/255, alpha: 1)
+    }
+    
+    static var idNftTextColor: UIColor {
+        return  UIColor(red: 240/255, green: 156/255, blue: 248/255, alpha: 1)
+    }
+    
+    static var nftPriceTextColor: UIColor {
+        return UIColor(red: 255/255, green: 152/255, blue: 255/255, alpha: 1)
+    }
+    
+    static var lastVizualizaTextColor: UIColor {
+        return UIColor(red: 255/255, green: 152/255, blue: 255/255, alpha: 1)
+    }
+    
+    static var textFieldBackgroundColor: UIColor {
+        return UIColor(red: 52/255, green: 52/255, blue: 52/255, alpha: 1.0)
+    }
+    
+    static var recoverPasswordTextColor: UIColor {
+        return UIColor(red: 231/255, green: 48/255, blue: 214/255, alpha: 1.0)
+    }
+    
+    static var signInMetamaskBorderColor: UIColor {
+        return UIColor(red: 207/255, green: 0/255, blue: 192/255, alpha: 1.0)
+    }
+}
